2457636e1dc331cb2e3cf00168898beb040166ae
Ronaldo Butrus <ronaldobutrus@gmail.com>
2021-12-26 17:59:30 +0000

    scaffolded identity

---
+++ TokenAuthDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TokenAuthDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
